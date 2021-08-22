c05b68cefd3c8a51896aad5ccfa3828fb70f0733
Agus Priyatin <aguspriyatin80>
2021-08-23 02:47:08 +0700

    part07 - desctructuring

---
+++ part07.js
@@ -0,0 +1,35 @@
+console.clear()
+
+// let name = "John Manuel Lark"
+// let nameArr = name.split(' ')
+// console.log(nameArr)
+
+// let firstName = nameArr[0]
+// let middleName = nameArr[1]
+
+// let [firstName, , lastName] = nameArr
+// let [firstName, , lastName] = nameArr
+
+// console.log(firstName)
+// console.log(middleName)
+// console.log(lastName)
+// console.log(lastName)
+
+// OBJECT
+// const person = {
+//     firstName: "John",
+//     middleName: "Manuel",
+//     lastName: "Lark",
+//     age: 20
+// }
+// const [firstName, lastName] = person
+
+let firstName = "John"
+let lastName = "Manuel"
+let age = 20
+
+const personObj = {
+    firstName,
+    lastName,
+    age
+}
