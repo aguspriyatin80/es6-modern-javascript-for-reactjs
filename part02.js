1a2150d9a270790fb5f7df5bfcc6dc6a6cfd81bf
Agus Priyatin <aguspriyatin80>
2021-08-23 00:53:23 +0700

    partt02 - variabel-creating-using-let-and-cost

---
+++ part02.js
@@ -0,0 +1,22 @@
+console.clear()
+
+let num1 = 10
+// let num1 = 20 // it will be error
+num1 = 20
+console.log(num1)
+
+let name = "Agus Priyatin"
+let isOldEnough = true
+let marks = [9, 10, 8.5]
+let blogObj = {
+    title: 'Blog Title 1',
+    description: 'Lorem ipsum dolor'
+}
+const num2 = 30;
+// num2 = 40 // it will be error
+console.log(num2)
+num3 = 50;
+console.log(num3)
+// var num3 // hoisted
+let num3 // get error
+
