89824f8ed37af2fdc2bd52fd2ecf8aca3822ff23
Agus Priyatin <aguspriyatin80>
2021-08-27 02:04:41 +0700

    part16 - chaining promises

---
+++ part16.js
@@ -0,0 +1,23 @@
+const postListPromise = new Promise((resolve, reject) => {
+    $.get("https://jsonplaceholder.typicode.com/posts", (data) => {
+        resolve(data)
+    }).fail(err => {
+        reject(new Error(`GET POST LIST FAILED and status ${err.status}`))
+    })
+})
+
+const postDetailsPromise = (data) => new Promise((resolve, reject) => {
+    $.get(`https://jsonplaceholder.typicode.com/posts/${data[0].id}`, (data) => {
+        resolve(data)
+    }).fail(err => {
+        reject(new Error(`DETAILS CALL FAILED and status ${err.status}`))
+    })
+})
+postListPromise
+    .then(postDetailsPromise)
+    .then(response => {
+        console.log('RESPONSE:', response)
+    })
+    .catch(err => {
+        console.log(err)
+    })
