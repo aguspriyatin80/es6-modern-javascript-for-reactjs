19fb44ff2cb781a56733195a16f1e0f8fe974c38
Agus Priyatin <aguspriyatin80>
2021-08-26 19:29:15 +0700

    part13 - inheritance in es6

---
+++ part13.js
@@ -0,0 +1,42 @@
+console.clear()
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    getName() {
+        return `My name is ${this.name}.`
+    }
+    getDetail() {
+        return `I am ${this.age} years old.`
+    }
+}
+
+class Pilot extends Person {
+    constructor(name, age, job) {
+        super(name, age, job)
+        this.name = "Eka"
+        this.job = job
+    }
+    getDetail() {
+        return "getDetail Pilot";
+    }
+    static getData() {
+        console.log('getData Pilot')
+    }
+    getJob() {
+        return `I am a ${this.job}`
+    }
+    getName() {
+        return `My name is ${this.name}.`
+    }
+    getData() {
+        return this.getName() + super.getDetail() + this.getJob()
+    }
+
+}
+const p = new Pilot("Agus", 40, "Programmer");
+console.log(p.getData())
+console.log(p.getDetail())
+Pilot.getData()
