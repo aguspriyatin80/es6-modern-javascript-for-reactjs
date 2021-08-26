2f0f441bd922b05796a38cbc93aec4441e492c1a
Agus Priyatin <aguspriyatin80>
2021-08-26 16:29:30 +0700

    part12 - introduction to Classes

--- part12.js
+++ part12.js
@@ -1,12 +1,26 @@
 console.clear()
 
-function Person(name, birthYear) {
+function Person(name, birthYear) { // function base
     this.name = name
     this.yearOfBirth = birthYear
     this.getDetails = function () {
         return `My name is ${this.name}, I'm ${2021 - this.yearOfBirth} years old`
     }
+    // return this
 }
 
+class Person2 { // class base
+    constructor(name, birthYear) {
+        this.name = name
+        this.birthYear = birthYear
+    }
+    getDetails = () => {
+        return `Name: ${this.name} and Age: ${2021 - this.birthYear}`
+    }
+}
 const p = new Person("Agus", 1980)
-console.log(p.getDetails())
+console.log(p.getDetails());
+const p2 = new Person2("Agus", 1980)
+console.log(p2.getDetails());
+
+// console.log(p.getDetails())
