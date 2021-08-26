b243083fe359d682e027b4af1418cc6ce50d59cd
Agus Priyatin <aguspriyatin80>
2021-08-27 03:00:01 +0700

    part14 - simple callback

--- part14.js
+++ part14.js
@@ -1,3 +1,16 @@
+// SIMPLE CALLBACK
+function main(param1, param2, cb) {
+    console.log(param1, param2)
+    cb()
+}
+
+function cb() {
+    console.log('hai callback')
+}
+
+main(1, 2, cb)
+
+// SIMPLE PROMISES  
 const postListPromise = new Promise((resolve, reject) => {
     $.get("https://jsonplaceholder.typicode.com/posts", (data) => {
         resolve(data)
